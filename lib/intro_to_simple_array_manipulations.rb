82cee27fd82571ffaa5c2eb1cce3122974c75dcc
Andrew  Perlis <ajperlis07@gmail.com>
2021-01-03 19:21:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,9 +10,8 @@ def using_unshift(bouroughs_in_nyc ,  new_neighborhood)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-def using_pop(continents)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+def using_pop(array)
+  array.pop
 end
 
 def pop_with_args(array)
